e1c5c89f8c244bb5e10706ba0ee554bf39da78ba
ABO0000 <albertevistep@gmail.com>
2021-11-19 19:14:51 +0400

    test

--- resources/js/Api.js
+++ resources/js/Api.js
@@ -7,8 +7,8 @@ const options = {
     },
     crossdomain: true
 };
-// options.baseURL = "https://admincrudpanel.herokuapp.com/api"; //for heroku
-options.baseURL = process.env.MIX_API_URL; //for local
+options.baseURL = "https://protected-everglades-43017.herokuapp.com/api"; //for heroku
+// options.baseURL = process.env.MIX_API_URL; //for local
 const instance = axios.create(options);
 instance.interceptors.response.use(response => {
     return response.data;
