a25f4f60d550c6267e715844226efa24e5497b45
ABO0000 <albertevistep@gmail.com>
2021-12-16 11:11:51 +0400

    make it betterfull

--- public/js/app.js
+++ public/js/app.js
@@ -2765,9 +2765,8 @@ function AllArticles() {
               style: {
                 width: '200px',
                 height: '200px',
-                background: 'red',
                 margin: '50px 10px 0 0',
-                backgroundImage: 'src("https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png")'
+                backgroundImage: 'url("https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png")'
               }
             }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)(react_router_dom__WEBPACK_IMPORTED_MODULE_4__.Link, {
               to: "/article/".concat(article.id),
